6b9cc01528472b4ed6a4b8ab494963fb73530c2e
Aaron Henry <aaronhenry1994@outlook.com>
2022-08-08 01:52:18 -0600

    I have gotten some functionality out of the btns.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Rock-Paper-Scissors
 I had some version working using a prompt method, but upon reading the instructions, I went through the process of making it more console based.
+# RPS-UI
+I am now going through the process of making it UI based. Here's hoping it's not as much of a headache.
--- main.js
+++ main.js
@@ -1,39 +1,48 @@
 // Choices of the game.
 const choices = ['rock', 'paper', 'scissors'];
 
-function playGame() {
-    for(let i = 0; i<=5; i++) {
-        let playerChoice = input = prompt('Type rock, paper, or scissors.');
-        playRound(playerChoice, computerChoice);
-    }
+let round = 0;
+
+let result;
+
+let winner;
+
+function roundCounter(){
+        round += 1;
 }
+
+const btn = document.querySelectorAll('#btn');
+btn.forEach((btn) => {
+        btn.addEventListener('click', () => {
+                console.log();
+        });
+});
+
+document.getElementById('whoWon').addEventListener('click', function() {
+    document.getElementById('returnStatement').textContent = "";
+})
 
 function computerChoice() {
-    let randomIndex = Math.floor(Math.random() * choices.length);
-
-        // Get random Index
-        const item = choices[randomIndex];
-        
-        return item;
+    Math.random(Math.floor() * choices.length);
+        return console.log(random, choices[random]);
         
 }
 
-function playRound(playerChoice, computerChoice) {{
-    // Setting up playerChoice vs randomIndex
-    if (playerChoice === computerChoice) {
+function whoWon() {
+        playGame();
+                return winner;
+}
+
+function playGame(computerChoice) {
+        if (btn === computerChoice) {
         return console.log('This is a tie.');
-    } else if (playerChoice === null) {
-        return console.log('Maybe next time.');
-    } else if (playerChoice === 'rock' && computerChoice === 'scisssors') {
-        return console.log('Congratuations, you win!');
-    } else if (playerChoice === 'paper' && computerChoice === 'rock') {
-        return console.log('Congratulations, you win!');
-    } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
-        return console.log('Congratulations, you win!');
+    } else if (btn === 'rock' && computerChoice === 'scissors') {
+        return console.log('This is a win!');
+    } else if (btn === 'paper' && computerChoice === 'rock') {
+        return console.log('This is a win!');
+    } else if (btn === 'scissors' && computerChoice === 'paper') {
+        return console.log('This is a win!');
     } else {
-        return console.log('Sorry, the computer wins.');
+        return console.log('The computer wins!');
+    }
 }
-}
-}
-
-playGame()

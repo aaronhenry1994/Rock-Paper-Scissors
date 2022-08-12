6fa4f16ef1ce20dc6b9b4eef28dfedf449f3e507
Aaron Henry <aaronhenry1994@outlook.com>
2022-08-12 13:58:18 -0600

    I feel like I am one line away from finishing.

--- main.js
+++ main.js
@@ -1,101 +1,85 @@
 // Choices of the game.
 const choices = ['rock', 'paper', 'scissors'];
-const SELECTIONS = [
-{
-        name: 'rock',
-        beats: 'scissors'
-},
-{
-        name: 'paper',
-        beats: 'rock'
-},{
-        name: 'scissors',
-        beats: 'paper'
-},
 
-]
+console.log(choices)
+console.log(choices[0])
+console.log(choices[1])
+console.log(choices[2])
 
+const btn0 = document.querySelector('#btn0');
+const btn1 = document.querySelector('#btn1');
+const btn2 = document.querySelector('#btn2');
+
+
+
+btn0.addEventListener('click', playerChoice, computerChoice, () => {
+        (choices[0]);
+})
+
+btn1.addEventListener('click', playerChoice, computerChoice, () => {
+        console.log(choices[1]);
+})
+
+btn2.addEventListener('click', playerChoice, computerChoice, () => {
+        (choices[2]);
+})
 
 let round = 0;
 let playerScore = 0;
 let computerScore = 0;
 
-let result;
-
 let winner;
 
 function roundCounter(){
+
         round += 1;
 }
 
-const btn = document.querySelectorAll('[data-selection]');
-btn.forEach((btn) => {
-        btn.addEventListener('click', handleClick, e => {
-        playGame();
-        });
-});
 
-function handleClick() {
-        playGame(this.value)
-}
-
-document.getElementById('whoWon').addEventListener('click', handleClick, function() {
-    document.getElementById('returnStatement').textContent = "";
-})
-
-function playerChoice(btn) {
-        if (playerChoice == 'rock' && computerChoice == 'scissors') {
-                console.log('You won!');
+ function playerChoice(computerChoice) {
+        
+        if (playerChoice == choices[0] && computerChoice == choices[1]) {
+                return console.log('You won!');
         }
-        if (playerChoice == 'paper' && computerChoice == 'rock') {
-                console.log('You won!');
+        if (playerChoice === choices[1] && computerChoice === choices[0]) {
+                return console.log('You won!');
         }
-        if (playerChoice == 'scissors' && computerChoice == 'paper') {
-                console.log('You won!');
+        if (playerChoice == choices[2] && computerChoice === choices[1]) {
+                return console.log('You won!');
         }
-        if (playerChoice == computerChoice) {
-                console.log('This is a tie.');
+        if (playerChoice === computerChoice) {
+                return console.log('This is a tie.');
 };
 };
 
 function computerChoice() {
     Math.random(Math.floor() * choices.length);
-        if (choices == 0) {
-                return 'rock';
+        if (choices[0]) {
+                return console.log('rock');
         }
-        else if (choices == 1) {
-                return 'paper';
+        else if (choices[1]) {
+                return console.log('paper');
         }
-        else if (choices == 2) {
-                return 'scissors';
+        else if (choices[2]) {
+                return console.log('scissors');
         }
-        if (playerChoice == 'scissors' && computerChoice == 'rock') {
-                console.log('Computer wins!');
+        if (playerChoice === choices[2] && computerChoice === choices[0]) {
+                return console.log('Computer wins!');
         }
-        if (playerChoice == 'rock' && computerChoice == 'paper') {
-                console.log('Computer wins!');
+        if (playerChoice === choices[0] && computerChoice === choices[1]) {
+                return console.log('Computer wins!');
         }
-        if (playerChoice == 'paper' && computerChoice == 'scissors') {
-                console.log('Computer wins!');
+        if (playerChoice === choices[1] && computerChoice === choices[2]) {
+                return console.log('Computer wins!');
         }
-        if (playerChoice == computerChoice) {
-                console.log('This is a tie.');
+        if (playerChoice === computerChoice) {
+                return console.log('This is a tie.');
         }
 };
 
-function playRound(){
-        for (let round = 0; round < 5; round++) {
-                playerChoice();
-                computerChoice();
-        }
+function playGame(playerChoice, computerChoice) {
+        return
 }
 
-function playGame() {
-        playRound();
-};
-
-function whoWon() {
-        playGame();
-                return winner;
-};
+playGame;
 
